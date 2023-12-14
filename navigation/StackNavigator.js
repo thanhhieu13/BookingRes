06d11a80a0fa044826321fb35984ada760fa0fc1
nguyenthanhhieu1304 <20521328@gm.uit.edu.vn>
2023-12-14 15:26:29 +0700

    bottom tab

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -5,32 +5,88 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from "../screens/Login";
 import RegisterScreen from "../screens/Register";
 import HomeScreen from "../screens/Home";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 
 const StackNavigator = () => {
     const Stack = createNativeStackNavigator();
+    const Tab = createBottomTabNavigator();
+    function BottomTabs() {
+        return (
+            <Tab.Navigator>
+                <Tab.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{
+                        tabBarLabel: "Home",
+                        tabBarLabelStyle: { color: "#008E97" },
+                        headerShown: false,
+                        tabBarIcon: ({ focused }) =>
+                            focused ? (
+                                <Entypo name="home" size={24} color="#008E97" />
+                            ) : (
+                                <AntDesign name="home" size={24} color="black" />
+                            ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Near"
+                    component={HomeScreen}
+                    options={{
+                        tabBarLabel: "Home",
+                        tabBarLabelStyle: { color: "#008E97" },
+                        headerShown: false,
+                        tabBarIcon: ({ focused }) =>
+                            focused ? (
+                                <Entypo name="home" size={24} color="#008E97" />
+                            ) : (
+                                <AntDesign name="home" size={24} color="black" />
+                            ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Account"
+                    component={HomeScreen}
+                    options={{
+                        tabBarLabel: "Home",
+                        tabBarLabelStyle: { color: "#008E97" },
+                        headerShown: false,
+                        tabBarIcon: ({ focused }) =>
+                            focused ? (
+                                <Entypo name="home" size={24} color="#008E97" />
+                            ) : (
+                                <AntDesign name="home" size={24} color="black" />
+                            ),
+                    }}
+                />
+            </Tab.Navigator>
+        )
+    }
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen
-          name="Login"
-          component={LoginScreen}
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Register"
+                    component={RegisterScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+          name="Main"
+          component={BottomTabs}
           options={{ headerShown: false }}
         />
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
 export default StackNavigator
