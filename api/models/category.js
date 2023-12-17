da92c9fd6c70fa35da83541f63bb44c5a3a9a8e7
nguyenthanhhieu1304 <20521328@gm.uit.edu.vn>
2023-12-17 15:29:42 +0700

    const cate

--- api/models/category.js
+++ api/models/category.js
@@ -1,17 +1,28 @@
 const mongoose = require('mongoose');
 
+const nameEnum = ['Buffet', 'Lẩu', 'Nướng', 'Hải Sản', 'Món Hàn', 'Món Nhật', 'Món Việt', 'Món Chay', 'Món Âu'];
+const imgEnum =
+    [
+        '../../assets/img/buffet.jpg',
+        '../../assets/img/hot-pot.png',
+        '../../assets/img/barbecue.jpg',
+        '../../assets/img/lobster.jpg',
+        '../../assets/img/soju.jpg',
+        '../../assets/img/noodle.jpg',
+        '../../assets/img/noodle-2.jpg',
+        '../../assets/img/salad.jpg',
+        '../../assets/img/steak.jpg',
+    ];
 const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    maxlength: 200
-  },
-  image: {
-    type: String // Assuming you store the image URL
-  }
+    name: {
+        type: String,
+        required: true,
+        enum: nameEnum,
+    },
+    image: {
+        type: String,
+        enum: imgEnum,
+    }
 });
 
 const Category = mongoose.model('Category', categorySchema);
