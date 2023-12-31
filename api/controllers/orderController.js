8a7f0552c0f8399df1957353b88b7af8bd2def81
khoanguyen23 <khoahoang151@gmail.com>
2023-12-31 13:58:05 +0700

    remove booking hours that ordered

--- api/controllers/orderController.js
+++ api/controllers/orderController.js
@@ -7,14 +7,14 @@ module.exports = {
     placeOrder: async (req, res) => {
         try {
             const { restaurantId, userId, adults, children, date, selectedHour, note } = req.body;
-
+    
             const restaurant = await Restaurant.findById(restaurantId);
             const user = await User.findById(userId);
-
+    
             if (!restaurant || !user) {
                 return res.status(404).json({ message: 'Restaurant or user not found' });
             }
-
+    
             const order = new Order({
                 restaurant: restaurantId,
                 user: userId,
@@ -23,18 +23,25 @@ module.exports = {
                 date,
                 selectedHour,
                 note,
-                status: "Chờ xác nhận", 
-                services: "Đặt chỗ", 
+                status: "Chờ xác nhận",
+                services: "Đặt chỗ",
             });
-
+    
             await order.save();
-
+    
+            // lọc bookingHours đã đặt 
+            const updatedBookingHours = restaurant.bookingHours.filter(hour => hour !== selectedHour);
+            restaurant.bookingHours = updatedBookingHours;
+    
+            await restaurant.save();
+    
             res.status(201).json({ message: 'Order placed successfully', order });
         } catch (error) {
             console.error('Error placing order:', error);
             res.status(500).json({ message: 'Internal server error' });
         }
     },
+    
     getOrdersByUser: async (req, res) => {
         try {
             const userId = req.params.userId;
