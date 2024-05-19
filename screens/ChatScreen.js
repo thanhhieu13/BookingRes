77749c13f6dd81d32057d5a11fad0d89ad081d87
khoanguyen23 <khoahoang151@gmail.com>
2024-05-20 01:41:16 +0700

    [Add] Basic UI Chat Screen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useState, useCallback, useEffect } from "react";
+import {
+    GiftedChat,
+  } from "react-native-gifted-chat";
 
 const ChatScreen = () => {
-  return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
-  )
-}
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Xin chào quý khách hàng đã tin tưởng và ủng hộ ứng dụng đặt bàn của chúng tôi !! Nếu quý khách có thắc mắc gì xin hãy đặt câu hỏi, chúng tôi sẽ phản hồi trong thời gian sớm nhất",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://static.vecteezy.com/system/resources/previews/020/429/953/non_2x/admin-icon-vector.jpg",
+        },
+      },
+    ]);
+  }, []);
 
-export default ChatScreen
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+
+
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+     
+    />
+  );
+};
+
+export default ChatScreen;
